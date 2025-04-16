3b3c1d4d095a6f954100cb403ba01f26951fe92b
Devicii <62472550+asroq1@users.noreply.github.com>
2025-04-16 10:55:15 +0900

    fix: 라우팅 구조 변경

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '../components/AudioWorkletSTT.vue'
 import ReceiptView from '../views/ReceiptView.vue'
 import MeetingSummaryView from '../views/MeetingSummaryView.vue'
 import LoginView from '../views/LoginView.vue'
@@ -8,10 +8,16 @@ import { loadUserFromStorage } from '../services/userService'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: '',
+    //   component: MainView,
+    //   meta: { requiresAuth: true },
+    // },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/main',
+      name: 'main',
+      component: MainView,
       meta: { requiresAuth: true },
     },
     {
